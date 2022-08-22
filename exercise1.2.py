0d8144f87d365ce0a19649d752bcc2b9b0f66dab
hemanthreddy <2100030180@kluniversity.in>
2022-08-22 11:41:46 +0530

    python programs

---
+++ exercise1.2.py
@@ -0,0 +1,24 @@
+class rectangle():
+    l = int(input("Enter length of rectangle: "))
+    b = int(input("Enter breadth of rectangle: "))
+    area = l*b
+    perimeter = 2*(l+b)
+    print(area)
+    print(perimeter)
+class circle():
+    r = int(input("enter the radius of circle:"))
+    areacircle = 2*3.14*r
+    perimetercircle = 3.14*r*r
+    print(areacircle)
+    print(perimetercircle)
+class triangle():
+    length = int(input("Enter length of triangle: "))
+    breadth = int(input("Enter breadth of triangle: "))
+    height = int(input("Enter height of triangle: "))
+    areatriangle = 0.5*breadth*height
+    perimetertriangle = length+breadth+height
+    print(areatriangle)
+    print(perimetertriangle)
+
+
+
---
+++ prog3.py
@@ -0,0 +1,5 @@
+a=5
+print(a)
+a1=9.35
+print(a1)
+type(a1)
